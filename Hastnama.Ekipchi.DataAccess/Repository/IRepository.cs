ad5e9731eb1e57513aaf38b635fffbf81e5a8e97
Nosratiz <nimanosrati93@gmail.com>
2020-01-25 18:00:22 +0330

    add helper and generic repository

---
+++ Hastnama.Ekipchi.DataAccess/Repository/IRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Hastnama.Ekipchi.Common.Helper;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace Hastnama.Ekipchi.DataAccess.Repository
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        IQueryable<TEntity> GetAll();
+
+        IQueryable<TEntity> FindBy(Expression<Func<TEntity, bool>> predicate);
+
+        ValueTask<EntityEntry<TEntity>> AddAsync(TEntity entity);
+
+        void Add(TEntity entity);
+
+        Task AddRange(IList<TEntity> tEntities);
+
+        void Delete(TEntity entity);
+
+        void RemoveRange(IEnumerable<TEntity> entities);
+
+        void Edit(TEntity entity);
+
+        Task<PagedList<TEntity>> GetPagedAsync(int pageNumber, int pageSize, IQueryable<TEntity> query);
+    }
+}
---
+++ Hastnama.Ekipchi.DataAccess/Repository/Repository.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Hastnama.Ekipchi.Common.Helper;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace Hastnama.Ekipchi.DataAccess.Repository
+{
+    public class Repository<TContext, TEntity> : IDisposable,
+        IRepository<TEntity>
+        where TEntity : class
+        where TContext : DbContext, new()
+
+    {
+        public TContext Context { get; set; }
+
+        public Repository(TContext context)
+        {
+            Context = context;
+        }
+
+        public virtual ValueTask<EntityEntry<TEntity>> AddAsync(TEntity entity)
+        {
+            return Context.Set<TEntity>().AddAsync(entity);
+        }
+
+
+        public void Add(TEntity entity)
+        {
+            Context.Add(entity);
+        }
+
+        public virtual Task AddRange(IList<TEntity> tEntities)
+        {
+            return Context.Set<TEntity>().AddRangeAsync(tEntities);
+        }
+
+        public void Delete(TEntity entity)
+        {
+            Context.Set<TEntity>().Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<TEntity> entities)
+        {
+            Context.Set<TEntity>().RemoveRange(entities);
+        }
+
+        public void Edit(TEntity entity)
+        {
+            Context.Entry(entity).State = EntityState.Modified;
+        }
+
+        public IQueryable<TEntity> FindBy(Expression<Func<TEntity, bool>> predicate)
+        {
+            return Context.Set<TEntity>().Where(predicate);
+        }
+
+        public IQueryable<TEntity> GetAll()
+        {
+            return Context.Set<TEntity>();
+        }
+
+        public async Task<PagedList<TEntity>> GetPagedAsync(int pageNumber, int pageSize, IQueryable<TEntity> query)
+        {
+            if (pageSize <= 0)
+                pageSize = 10;
+
+            var rowsCount = query.Count();
+
+            if (rowsCount <= pageSize || pageNumber <= 1)
+                pageNumber = 1;
+
+            return await PagedList<TEntity>.CreateAsync(query, pageNumber, pageSize, rowsCount);
+        }
+
+
+        public PagedList<TEntity> GetPagedAsync(int pageNumber, int pageSize, IEnumerable<TEntity> query)
+        {
+            if (pageSize <= 0)
+                pageSize = 10;
+
+            var rowsCount = query.Count();
+
+            if (rowsCount <= pageSize || pageNumber <= 1)
+                pageNumber = 1;
+
+            return PagedList<TEntity>.CreateAsync(query, pageNumber, pageSize, rowsCount);
+        }
+        public void Dispose()
+        {
+            Context.Dispose();
+        }
+    }
+}
