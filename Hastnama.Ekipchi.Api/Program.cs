e37fcbebc37cbcab1b6b045c3078738b726e0c94
Nosratiz <nimanosrati93@gmail.com>
2020-02-05 11:38:53 +0330

    add seed data to program

--- Hastnama.Ekipchi.Api/Program.cs
+++ Hastnama.Ekipchi.Api/Program.cs
@@ -1,4 +1,7 @@
+using Hastnama.Ekipchi.Api.Data;
+using Hastnama.Ekipchi.DataAccess.Context;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 namespace Hastnama.Ekipchi.Api
@@ -7,6 +10,13 @@ namespace Hastnama.Ekipchi.Api
     {
         public static void Main(string[] args)
         {
+            using (var scope = CreateHostBuilder(args).Build().Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                services.GetRequiredService<EkipchiDbContext>();
+
+                SeedData.Initialize(services);
+            }
             CreateHostBuilder(args).Build().Run();
         }
 
