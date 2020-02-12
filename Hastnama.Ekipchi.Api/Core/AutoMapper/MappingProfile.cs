e8d796b646c43a865379552fbda6e116371f3152
javani <mohammad.javan.t@gmail.com>
2020-02-12 15:10:34 +0330

    fix  update event

--- Hastnama.Ekipchi.Api/Core/AutoMapper/MappingProfile.cs
+++ Hastnama.Ekipchi.Api/Core/AutoMapper/MappingProfile.cs
@@ -236,6 +236,7 @@ namespace Hastnama.Ekipchi.Api.Core.AutoMapper
 
 
             CreateMap<UpdateEventDto, Event>()
+                .ForMember(x => x.EventGallery, opt => opt.Ignore())
                 .ForMember(x => x.Image, opt => opt.MapFrom(o => o.Logo));
 
             CreateMap<Event, EventDto>()
